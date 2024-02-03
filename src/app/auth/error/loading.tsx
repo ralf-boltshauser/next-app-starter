e7fb4eacc2d6caa240a75974cc267bd42de62db5
Ralf Boltshauser <ralf@boltshauser.com>
2024-02-03 20:19:19 +0100

    update

---
+++ src/app/auth/error/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div>
+      <h2 className="text-2xl">Loading</h2>
+    </div>
+  );
+}
--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -7,10 +7,10 @@ export default function ErrorPage() {
   const errorMessage = useSearchParams().get("error");
   return (
     <div>
-      <h2 className="text-2xl">Etwas ist schief gegangen!</h2>
+      <h2 className="text-2xl">Something went wrong!</h2>
       <p className="my-3">{errorMessage}</p>
       <Button variant="outline" onClick={() => window.history.back()}>
-        Zurück
+        Back
       </Button>
     </div>
   );
