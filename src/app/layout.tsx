16472ead1905c970e985e90ed78f95aac9f39653
Ralf Boltshauser <ralf@boltshauser.com>
2024-02-11 15:35:27 +0100

    add toaster by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import NextAuthProvider from "@/components/auth/NextAuthProvider";
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({
             <div className="m-5">{children}</div>
           </main>
         </NextAuthProvider>
+        <Toaster />
       </body>
     </html>
   );
